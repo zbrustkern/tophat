d9e0fb3031f895db8b8b40875b6e5867a3d409a6
User Name <zbrustkern@gmail.com>
2024-08-20 15:39:57 -0700

    added a card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import { Button } from "@/components/ui/button"
+import { Button, } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
     <main>
           <div>
             <Button>Click me</Button>
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
           </div>
     </main>
   );
