229c18497bf34985667cd1d827089ee0d3b01737
User Name <zbrustkern@gmail.com>
2024-08-26 10:11:06 -0700

    dropped from next image to img src

--- src/components/ui/tophat-logo.tsx
+++ src/components/ui/tophat-logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function TophatLogo() {
     return (
         <div className="text-orange-300">
-        <Image src="/tophat_logo.png"
+        <img src="/tophat_logo.png"
                     width={175}
                     height={355}
                     className=""
