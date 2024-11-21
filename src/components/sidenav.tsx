b463c9a2579fa028a287195ef9a95f60765b4f16
User Name <zbrustkern@gmail.com>
2024-11-21 14:44:12 -0800

    sidenav import fixed

--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
 export function SideNav() {
